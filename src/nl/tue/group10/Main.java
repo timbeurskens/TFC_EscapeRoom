314ceb4dfc1c2fdce5772db52c3f58412c1c8381
Tim Beurskens <timbeurskens.97@gmail.com>
2018-02-28 13:41:50 +0100

    Add processing & oocsi libraries, create functional main class

--- src/nl/tue/group10/Main.java
+++ src/nl/tue/group10/Main.java
@@ -1,9 +1,13 @@
 package nl.tue.group10;
 
+import processing.core.PApplet;
+
 /**
  * TFC_EscapeRoom
  * Created by s154796 on 28-2-2018.
  */
 public class Main {
-
+    public static void main(String[] args) {
+        PApplet.main("nl.tue.group10.module.LockModule");
+    }
 }
---
+++ src/nl/tue/group10/module/LockModule.java
@@ -0,0 +1,21 @@
+package nl.tue.group10.module;
+
+import processing.core.PApplet;
+
+/**
+ * TFC_EscapeRoom
+ * Created by s154796 on 28-2-2018.
+ */
+public class LockModule extends PApplet {
+    public void setup() {
+
+    }
+
+    public void draw() {
+        background(0);
+    }
+
+    public void settings() {
+        size(800, 800, P2D);
+    }
+}
